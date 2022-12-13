316a297b51ef7bac67246a7f53e05f2d21385cb6
vlaksy <petrovic.pr102.2018@uns.ac.rs>
2022-12-13 18:34:45 +0100

    Uputstvo

--- home.py
+++ home.py
@@ -1,8 +1,37 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+from mysql import connector
+
+# SVI URADITE OVO U TERMINALU DA BI RADILA BAZA KADA POKRENETE
+# pip install mysqlclient
+# pip install mysql-python
+
+# AKO I DALJE NE RADI IDITE REDOM I DODAJTE JEDAN PO JEDAN DOK NE PRORADI
+# mysql-connector
+# mysql-connector-python
+# mysql-connector-python-rf
 
 
 main = Flask(__name__)
+
+main.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@localhost/users'
+# Initializing the database
+db = SQLAlchemy(main)
+
+# dodaj ovde db.create_all()
+
+
+# Creating db model
+class Database(db.Model):
+    name = db.Column(db.String(15), nullable=False)
+    surname = db.Column(db.String(15), nullable=False)
+    date = db.Column(db.DateTime)
+    email = db.Column(db.Email, primary_key=True)
+
+
+# Create a function to return a string when we add someone
+def __repr__(self):
+    return '<Name %r>' % self.email
 
 
 @main.route('/')
