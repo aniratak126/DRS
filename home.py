33fe71742830e846f87459d54b0261f5de25d220
vlaksy <petrovic.pr102.2018@uns.ac.rs>
2022-12-13 19:28:03 +0100

    Ispravka

--- home.py
+++ home.py
@@ -12,6 +12,7 @@ main.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 db = SQLAlchemy(main)
 
 # ovo odkomentarisati samo prvi put kada pokrecete (valjda)
+# i onda bi trebalo da vam se pojavi folder instance u kome je users.db
 with main.app_context():
     db.create_all()
 
