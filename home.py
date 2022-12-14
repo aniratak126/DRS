0126a20ea266aba114972e90bdb17279ecaa0b37
Luka Zdravkovic <zdravkovicc.lukaa@gmail.com>
2022-12-13 17:53:03 -0800

    Register page added

--- home.py
+++ home.py
@@ -1,9 +1,19 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+DB_NAME = "datebase.db"
+
 main = Flask(__name__)
+
 
 @main.route('/')
 def index():
     return render_template('home.html')
+
+@main.route('/register')
+def reg():
+    return render_template('register.html')
 
 
 @main.route('/get-name', methods=['POST'])
