d26c5db69ba69da4ee1e3402c8082267068f008f
vlaksy <petrovic.pr102.2018@uns.ac.rs>
2022-12-15 22:05:53 +0100

    Reforming

--- run.py
+++ run.py
@@ -4,3 +4,8 @@ if __name__ == '__main__':
     app.run()
     # with app.app_context():
     #     db.create_all()
+
+# U PRVOJ LINIJI ODKOMENTARISATI # ,db
+# U PETOJ I SESTOJ LINIJI ODKOMENTARISATI SVE
+# ZAKOMENTARISATI CETVRTU LINIJU
+# POKRENI JEDNOM, NAPRAVI SE TABELA, VRATI KAKO JE BILO
