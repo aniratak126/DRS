6bdc88d44c175b2bbca5e0b7ef50b60e900700d2
Aleksa001 <astokic20017@gmail.com>
2023-01-25 14:39:33 +0100

    Dodata sva polja za update account.

--- run.py
+++ run.py
@@ -1,12 +1,12 @@
 from CryptoProject import create_app
-# from CryptoProject import db
+#from CryptoProject import db
 app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
 
-     # with app.app_context():
-        # db.create_all()
+      #with app.app_context():
+         #db.create_all()
 
 # INSTRUCTIONS
 # itsdangerous -> Flask-Bcrypt -> Flask-Login -> Flask-Mail -> Flask-WTF -> Mail -> Pillow -> PyJWT -> WTForms
