43e71c050ee7c18678fe861329a51800f2db525d
Aleksa001 <astokic20017@gmail.com>
2023-01-25 18:16:50 +0100

    Proba.

--- CryptoProject/users/routes.py
+++ CryptoProject/users/routes.py
@@ -28,7 +28,6 @@ def login():
         return redirect(url_for('main.home'))
     elif current_user.is_authenticated:
         return redirect(url_for('verification'))
-
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
