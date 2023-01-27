d43945932295fa37f8f97f148af8d3c92319bd23
vlaksy <petrovic.pr102.2018@uns.ac.rs>
2023-01-27 17:04:35 +0100

    Izmena tabele

--- CryptoProject/transactions/routes.py
+++ CryptoProject/transactions/routes.py
@@ -20,7 +20,26 @@ def new_transaction():
         return redirect(url_for('users.verification'))
     form = TransactionForm()
     if form.validate_on_submit():
-        if current_user.money >= form.amount.data:
+        if form.currency.data == 'money':
+            helpvar = current_user.money
+            helpvar2 = 'money'
+        elif form.currency.data == 'bitcoin':
+            helpvar = current_user.bitcoin
+            helpvar2 = 'bitcoin'
+        elif form.currency.data == 'dogecoin':
+            helpvar = current_user.dogecoin
+            helpvar2 = 'dogecoin'
+        elif form.currency.data == 'litecoin':
+            helpvar = current_user.litecoin
+            helpvar2 = 'litecoin'
+        elif form.currency.data == 'ripple':
+            helpvar = current_user.ripple
+            helpvar2 = 'ripple'
+        else:
+            helpvar = current_user.ethereum
+            helpvar2 = 'ethereum'
+
+        if helpvar >= form.amount.data:
             flash('Your transaction is being processed! After 5 minutes check your Transaction History'
                   ' for information about your transaction', 'success')
             k = keccak.new(digest_bits=256)
@@ -29,13 +48,13 @@ def new_transaction():
                     "<f", form.amount.data) + bytes(randint(1, 99999)))
             transaction_id = k.hexdigest()
             transaction = Transaction(id=transaction_id, sender_id=current_user.email, receiver_id=form.email.data,
-                                      amount=form.amount.data, status=Status.IN_PROGRESS.name)
+                                      amount=form.amount.data, status=Status.IN_PROGRESS.name, currency=helpvar)
             db.session.add(transaction)
             db.session.commit()
             sender = current_user.email
             try:
-                threading.Thread(target=transaction_thread, args=(form.email.data,
-                                                                  form.amount.data, transaction_id, sender)).start()
+                threading.Thread(target=transaction_thread, args=(form.email.data, form.amount.data,
+                                                                  transaction_id, sender, helpvar2)).start()
             finally:
                 return redirect(url_for('users.logged'))
         else:
@@ -44,7 +63,7 @@ def new_transaction():
                 bytes(current_user.email, encoding='utf-8') + bytes(form.email.data, encoding='utf-8') + struct.pack(
                     "<f", form.amount.data) + bytes(randint(1, 99999)))
             transaction = Transaction(id=k.hexdigest(), sender_id=current_user.email, receiver_id=form.email.data,
-                                      amount=form.amount.data, status=Status.DENIED.name)
+                                      amount=form.amount.data, status=Status.DENIED.name, currency=helpvar)
             db.session.add(transaction)
             db.session.commit()
             flash('Insufficient funds!', 'danger')
@@ -80,23 +99,72 @@ def transaction_history():
     return render_template('history.html', historySend=historySend, historyRecv=historyRecv, verified=True)
 
 
-def transaction_thread(email, amount, transaction_id, sender):
+def transaction_thread(email, amount, transaction_id, sender, helpvar):
     from run import app
-    time.sleep(5)
+    time.sleep(2)
     try:
         with app.app_context():
             user = User.query.filter_by(email=email).first()
             i_did_it = User.query.filter_by(email=sender).first()
             transaction_done = Transaction.query.filter_by(id=transaction_id).first()
-            if i_did_it.money - amount < 0:
-                transaction_done.status = Status.DENIED.name
-            else:
-                user.money = user.money + amount
-                i_did_it.money = i_did_it.money - amount
-                transaction_done.status = Status.COMPLETED.name
+            if helpvar == 'money':
+                if i_did_it.money - amount < 0:
+                    transaction_done.status = Status.DENIED.name
+                    transaction_done.currency = 'dollar'
+                else:
+                    user.money = user.money + amount
+                    i_did_it.money = i_did_it.money - amount
+                    transaction_done.status = Status.COMPLETED.name
+                    transaction_done.currency = helpvar
+            elif helpvar == 'bitcoin':
+                if i_did_it.bitcoin - amount < 0:
+                    transaction_done.status = Status.DENIED.name
+                    transaction_done.currency = helpvar
+                else:
+                    user.bitcoin = user.bitcoin + amount
+                    i_did_it.bitcoin = i_did_it.bitcoin - amount
+                    transaction_done.status = Status.COMPLETED.name
+                    transaction_done.currency = helpvar
+            elif helpvar == 'dogecoin':
+                if i_did_it.dogecoin - amount < 0:
+                    transaction_done.status = Status.DENIED.name
+                    transaction_done.currency = helpvar
+                else:
+                    user.dogecoin = user.dogecoin + amount
+                    i_did_it.dogecoin = i_did_it.dogecoin - amount
+                    transaction_done.status = Status.COMPLETED.name
+                    transaction_done.currency = helpvar
+            elif helpvar == 'litecoin':
+                if i_did_it.litecoin - amount < 0:
+                    transaction_done.status = Status.DENIED.name
+                    transaction_done.currency = helpvar
+                else:
+                    user.litecoin = user.litecoin + amount
+                    i_did_it.litecoin = i_did_it.litecoin - amount
+                    transaction_done.status = Status.COMPLETED.name
+                    transaction_done.currency = helpvar
+            elif helpvar == 'ripple':
+                if i_did_it.ripple - amount < 0:
+                    transaction_done.status = Status.DENIED.name
+                    transaction_done.currency = helpvar
+                else:
+                    user.ripple = user.ripple + amount
+                    i_did_it.ripple = i_did_it.ripple - amount
+                    transaction_done.status = Status.COMPLETED.name
+                    transaction_done.currency = helpvar
+            elif helpvar == 'ethereum':
+                if i_did_it.ethereum - amount < 0:
+                    transaction_done.status = Status.DENIED.name
+                    transaction_done.currency = helpvar
+                else:
+                    user.ethereum = user.ethereum + amount
+                    i_did_it.ethereum = i_did_it.ethereum - amount
+                    transaction_done.status = Status.COMPLETED.name
+                    transaction_done.currency = helpvar
             db.session.commit()
     except:
         with app.app_context():
             transaction_done = Transaction.query.filter_by(id=transaction_id).first()
             transaction_done.status = Status.DENIED.name
+            transaction_done.currency = helpvar
             db.session.commit()
