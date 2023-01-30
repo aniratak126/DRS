260887cdbe598320c6b961cd2ea341f785f87e3d
vlaksy <petrovic.pr102.2018@uns.ac.rs>
2023-01-30 23:07:36 +0100

    Poslednji push

--- run.py
+++ run.py
@@ -2,13 +2,13 @@ from CryptoProject import create_app
 # from CryptoProject import db
 app = create_app()
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+app.run(host='0.0.0.0', port=8080)
 
-     # with app.app_context():
+    # with app.app_context():
         # db.create_all()
+
 
 # INSTRUCTIONS
 # itsdangerous -> Flask-Bcrypt -> Flask-Login -> Flask-Mail ->
 # Flask-WTF -> Mail -> Pillow -> PyJWT -> WTForms -> email-validator
-
