f549d58591ad541328951e430f084ab7346fe08e
vlaksy <petrovic.pr102.2018@uns.ac.rs>
2023-01-31 00:32:42 +0100

    Poslednji push

--- run.py
+++ run.py
@@ -2,8 +2,8 @@ from CryptoProject import create_app
 # from CryptoProject import db
 app = create_app()
 
-# if __name__ == '__main__':
-app.run(host='0.0.0.0', port=8080)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
 
     # with app.app_context():
         # db.create_all()
